763c70618ca8d69417410e87232770da8b637dc2
Mariam Lukava <mariamlukava@gmail.com>
2024-08-26 15:04:19 +0400

    add index and script

---
+++ main.js
@@ -0,0 +1,89 @@
+// task 1
+let firstName = "Mariam";
+console.log("My name is " + firstName + ".");
+
+// task 2
+let x = "5"; // string
+let y = 5;  //number
+
+console.log(typeof x, typeof y);
+
+// task 3
+let age = 28
+console.log(`I am ${age} years old.`);
+
+// task 4
+let isStudent = true;
+console.log(isStudent);
+
+//task 5
+//comparison operators give boolean results
+
+//task 6
+let text = "Hello";
+text.toUpperCase();
+console.log(text.toUpperCase());
+
+// task 7
+text = "World";
+text.toLowerCase();
+console.log(text.toLowerCase());
+
+//task 8 
+text = 'Hello';
+text.toUpperCase();
+console.log(text);
+//value before - Hello, value after method - HELLO
+
+// task 9
+text;
+console.log(text);
+// value of the text - undefined
+
+// task 10
+text = 'Hello';
+text.toLowerCase;
+console.log(text.toLowerCase());
+
+// task 11
+let age1 = Number(prompt('How old are you?'));
+let nextAge = age1 + 1;   //is itt?
+console.log(`Next year you will be ${nextAge}`);
+
+// task 12
+let name = prompt('What is your name?');
+let age2 = Number(prompt('How old are you?'));
+let isInSixties = (60 <= age2);
+console.log(`${name} is in sixties: ${isInSixties}`);
+
+// task 13
+x = 5;
+console.log(x++);  //5
+console.log(x);   //6
+
+// task 14
+y = 5;
+console.log(++y);  //6
+console.log(y);  //6
+
+// task 15
+// x++   increment(x+1)
+// x--   decrement(x-1)
+
+
+// task 16
+// ++x preincrement(add 1 to x then give value of x)
+//  x++ postincrement(give value of x then add 1 to the value)
+
+
+// task 17
+// value - undefined
+
+//task 18
+console.log(5 == 5);
+
+//task 19
+console.log(5 != 4);
+
+//task 20
+// string + number = implicit conversion of number to string. result is string
